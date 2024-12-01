5ee8ee717ace3de364c03e38e38105984c35ae9b
mjim0 <mdjim0910@gmail.com>
2024-11-30 19:49:48 -0500

    changed param for userlist, altered likemessage, stub for directmessages

--- static/js/functions.js
+++ static/js/functions.js
@@ -8,7 +8,7 @@ socket.on('invalidLOGIN', function (data) {
 });
 
 
-socket.on('userLIST', function (data) {
+socket.on('userLIST', function (userList) {
 });
 
 socket.on('postINFO', function (data) {
@@ -22,21 +22,19 @@ socket.on('directMessage', function (data) {
 });
 
 
-
+//button ufnctionality somehwere here
 
 
 function likeMessage(postID) {
     console.log("Here's your post: " + postID);
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            console.log(this.response);
-        }
-    };
-    request.open("POST", "/like");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify({postID: postID}));
+    socket.emit('likePost', {postID:postID});
+
 }
+
+function sendDirectMessage(recipient, message) {
+    socket.emit('directMessage', "notsure");
+}
+
 
 function displayText() {
     document.getElementById("description").innerHTML += "<br/>If you are seeing this, then displayText() in functions.js is doing it's job! 😀";
