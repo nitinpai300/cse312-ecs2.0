a74208a2d6e7069772303e39acb75fcd32965294
mjim0 <mdjim0910@gmail.com>
2024-12-01 01:56:57 -0500

    trying to do js aspect of websocket wwhere based off stack, i should be updating/getting html elements here and updating it, currenrlt did userlist, will try likecount

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,22 +1,33 @@
 const socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('validLOGIN', function (data) {
+    console.log('You are currently logged in as: ' + data.username);
 });
-
 
 socket.on('invalidLOGIN', function (data) {
+    console.log('could not login');
+});
+//if userlist exists, empty it so it tecnically is a refresh, and add every user from userlist in list form li
+socket.on('userLIST', function (userLIST) {
+    console.log("User List: ", userLIST);
+    const userLISTHTML = document.getElementById('userLIST');
+    if (userLISTHTML) {
+        userLISTHTML.innerHTML ="";
+        for (let i = 0; i < userLIST.length; i++) {
+            const aUser = document.createElement('li');
+            aUser.textContent = userLIST[i];
+            userLISTHTML.appendChild(aUser);
+        }
+    }
 });
 
-
-socket.on('userLIST', function (userList) {
-});
-
+//i have no clue
 socket.on('postINFO', function (data) {
 });
 
+//if like count exists, set like count equal to thelike count from post
 socket.on('updateLikeCount', function (data) {
 });
-
 
 socket.on('directMessage', function (data) {
 });
