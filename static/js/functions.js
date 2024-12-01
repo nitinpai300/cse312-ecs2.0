b64bcc0e9a53e8df0b3e1f879becac55fe50ca58
mjim0 <mdjim0910@gmail.com>
2024-11-30 19:46:26 -0500

    based on emits from py file, js file should update on that(?)

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,4 +1,30 @@
-// by chris
+const socket = io.connect('http://' + document.domain + ':' + location.port);
+
+socket.on('validLOGIN', function (data) {
+});
+
+
+socket.on('invalidLOGIN', function (data) {
+});
+
+
+socket.on('userLIST', function (data) {
+});
+
+socket.on('postINFO', function (data) {
+});
+
+socket.on('updateLikeCount', function (data) {
+});
+
+
+socket.on('directMessage', function (data) {
+});
+
+
+
+
+
 function likeMessage(postID) {
     console.log("Here's your post: " + postID);
     const request = new XMLHttpRequest();
@@ -15,3 +41,5 @@ function likeMessage(postID) {
 function displayText() {
     document.getElementById("description").innerHTML += "<br/>If you are seeing this, then displayText() in functions.js is doing it's job! 😀";
 }
+
+
